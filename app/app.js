ffca0a703dc60c4b12bc07579526cad53ea70027
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-12-30 07:25:13 -0300

    creacion de productos

---
+++ app/app.js
@@ -0,0 +1,18 @@
+const productos[
+    {
+        nombre: "Lentes 3D",
+        imagen: "./images/Len1",
+        precio: 90
+    },
+    {
+        nombre: "Perfume 9",
+        imagen: "./images/Per9,
+        precio: 450
+    },
+    {
+        nombre: "Bandeja para Papas",
+        imagen: "./images/Ban2",
+        precio: 150
+    }
+];
+
