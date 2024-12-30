2979a8c33860a5d9118848301f7cc4c5da153763
Migue <miguelulmi@hotmail.com>
2024-12-30 12:32:37 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,14 +1,17 @@
 const productos = [
     {
         nombre: "Lentes 3D",
+        imagen: "./images/Len1",
         precio: 90
     },
     {
         nombre: "Perfume 9",
+        imagen: "./images/Per9",
         precio: 450
     },
     {
         nombre: "Bandeja para Papas",
+        imagen: "./images/Ban2",
         precio: 150
     }
 ];
@@ -56,6 +59,7 @@ function pagarTotalCarrito() {
     }
 }
 
+crearCardsConDatos();
 crearListenersBotonesAgregar();
 botonBorrar.addEventListener("click", borrarCarrito);
 botonPagar.addEventListener("click", pagarTotalCarrito);
