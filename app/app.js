632ed3d9b2394d5e648406ed82d1e97dfb5c8fbe
Migue <miguelulmi@hotmail.com>
2024-12-30 11:42:34 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,29 +1,62 @@
-const productos[
+const productos = [
     {
         nombre: "Lentes 3D",
-        imagen: "./images/Len1",
         precio: 90
     },
     {
         nombre: "Perfume 9",
-        imagen: "./images/Per9,
         precio: 450
     },
     {
         nombre: "Bandeja para Papas",
-        imagen: "./images/Ban2",
         precio: 150
     }
 ];
 
 let productosCards = "";
 
-const contenedorProductos = document.getElementById("contenedor-productos");
-const listaCarrito = document.getElementById("carrito-lista");
-const totalCarrito = document.getElementById("carrito-total")
-const botonBorrar = document.getElementById("carrito-boton-borrar");
-const botonPagar = document.getElementById("carrito-boton-pagar");
-const textoPagarCarrito = document.getElementById("mensajeCarrito");
+let contenedorProductos = document.getElementById("contenedor-productos");
+let listaCarrito = document.getElementById("carrito-lista");
+let totalCarrito = document.getElementById("carrito-total")
+let botonBorrar = document.getElementById("carrito-boton-borrar");
+let botonPagar = document.getElementById("carrito-boton-pagar");
+let textoPagarCarrito = document.getElementById("mensajeCarrito");
 
 let totalAPagar = 0;
 
+function crearListenersBotonesAgregar() {
+
+    const botonAgregarProducto = document.querySelectorAll(".productos__caja__boton");
+
+    console.log(botonAgregarProducto.length);
+
+    for (let indice = 0; indice < botonAgregarProducto.length; indice++) {
+        
+        function agregarElemCarrito() {
+            const elementoLi = document.createElement("li");
+
+            elementoLi.innerText = `Caja ${productos[indice].nombre} a $ ${productos[indice].precio} c/u`;
+            listaCarrito.appendChild(elementoLi);
+            totalAPagar += productos[indice].precio;
+            totalCarrito.innerText = "Total a Pagar: $" + totalAPagar;
+            textoPagarCarrito.innerText = "";
+        }
+
+        botonAgregarProducto[indice].addEventListener("click", agregarElemCarrito);
+    }
+}
+
+function borrarCarrito() {
+    listaCarrito.innerHTML = "";
+    totalCarrito.innerHTML = "Total a Pagar: $0";
+    totalAPagar = 0;
+    textoPagarCarrito.innerText = "";
+}
+
+function pagarTotalCarrito() {
+    if (listaCarrito.innerText === "") {
+        textoPagarCarrito.innerText = "No has seleccionado ningún producto";
+    } else {
+        window.location.href = "pagos.html";
+    }
+}
