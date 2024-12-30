cc8c0c5af1fcecc1d89bd6784e2ae2efed6513f3
Migue <miguelulmi@hotmail.com>
2024-12-30 12:01:43 -0300

    Agregada funcion de agregar al carrito cada producto

--- app/app.js
+++ app/app.js
@@ -20,7 +20,6 @@ let listaCarrito = document.getElementById("carrito-lista");
 let totalCarrito = document.getElementById("carrito-total")
 let botonBorrar = document.getElementById("carrito-boton-borrar");
 let botonPagar = document.getElementById("carrito-boton-pagar");
-let textoPagarCarrito = document.getElementById("mensajeCarrito");
 
 let totalAPagar = 0;
 
@@ -28,18 +27,16 @@ function crearListenersBotonesAgregar() {
 
     const botonAgregarProducto = document.querySelectorAll(".productos__caja__boton");
 
-    console.log(botonAgregarProducto.length);
-
     for (let indice = 0; indice < botonAgregarProducto.length; indice++) {
         
         function agregarElemCarrito() {
             const elementoLi = document.createElement("li");
+            const elementoImg = document.createElement("img");
 
             elementoLi.innerText = `Caja ${productos[indice].nombre} a $ ${productos[indice].precio} c/u`;
             listaCarrito.appendChild(elementoLi);
             totalAPagar += productos[indice].precio;
             totalCarrito.innerText = "Total a Pagar: $" + totalAPagar;
-            textoPagarCarrito.innerText = "";
         }
 
         botonAgregarProducto[indice].addEventListener("click", agregarElemCarrito);
@@ -50,13 +47,16 @@ function borrarCarrito() {
     listaCarrito.innerHTML = "";
     totalCarrito.innerHTML = "Total a Pagar: $0";
     totalAPagar = 0;
-    textoPagarCarrito.innerText = "";
 }
 
 function pagarTotalCarrito() {
     if (listaCarrito.innerText === "") {
-        textoPagarCarrito.innerText = "No has seleccionado ningún producto";
+        totalCarrito.innerText = "No has seleccionado ningún producto";
     } else {
-        window.location.href = "pagos.html";
+        window.location.href = "pago.html";
     }
 }
+
+crearListenersBotonesAgregar();
+botonBorrar.addEventListener("click", borrarCarrito);
+botonPagar.addEventListener("click", pagarTotalCarrito);
